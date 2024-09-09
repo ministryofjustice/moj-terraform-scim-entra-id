add342acbbeb7d2998fa9d29017442cfea62ad17
julialawrence <julia.lawrence@digital.justice.gov.uk>
2024-09-09 18:42:25 +0100

    Refactoring how the function reads the event strings and logging the mode

--- function/app.py
+++ function/app.py
@@ -669,10 +669,10 @@ def lambda_handler(event, context):  # pylint: disable=W0621,W0613
     Returns:
         dict: HTTP response with status code and body.
     """
-    dry_run = event.get("dry_run", True)
+    dry_run = event.get("dry_run", "True").lower() == "true"
+    logger.info("Running in dry_run = %s mode", dry_run)
     sso_client = boto3.client("sso-admin", region_name="eu-west-2")
     identity_center_client = boto3.client("identitystore", region_name="eu-west-2")
-
     identity_store_id = get_identity_store_id(sso_client)
 
     try:

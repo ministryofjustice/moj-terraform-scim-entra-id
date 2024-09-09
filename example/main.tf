daceb7f82709cb5fc267f5f451221f184d422465
julialawrence <julia.lawrence@digital.justice.gov.uk>
2024-09-09 14:42:54 +0100

    Updating readme and adding example

---
+++ example/main.tf
@@ -0,0 +1,12 @@
+provider "aws" {
+  region = "eu-west-2" # Replace with your desired AWS region
+}
+
+module "entra_id_scim_lambda" {
+  source = "../."
+
+  # Required variables for the module
+  azure_tenant_id     = "your-tenant-id"     # Replace with your Azure Tenant ID
+  azure_client_id     = "your-client-id"     # Replace with your Azure Client ID
+  azure_client_secret = "your-client-secret" # Replace with your Azure Client Secret
+}

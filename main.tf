d0f7a74f60e34310ecad73794e06fc1b1fb61dfa
Aaron Robinson <aaron.robinson1@justice.gov.uk>
2026-07-01 09:17:30 +0100

    Bump dependencies

--- main.tf
+++ main.tf
@@ -104,7 +104,7 @@ resource "aws_lambda_function" "default" {
   function_name = local.name
   role          = aws_iam_role.default.arn
   handler       = "app.lambda_handler"
-  runtime       = "python3.11"
+  runtime       = "python3.13"
   timeout       = 300
 
   filename         = data.archive_file.function.output_path
